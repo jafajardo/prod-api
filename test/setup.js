4fc7b80b2a5943356b6b2bfe508ecdf48a4ba91c
Joseph <jafajardo@gmail.com>
2021-09-27 16:13:33 +1000

    Update Jest setup file

--- test/setup.js
+++ test/setup.js
@@ -1,24 +1,26 @@
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const mongoose = require('mongoose');
 
-let mongo;
-beforeAll(async () => {
-  process.env.JWT_KEY = '7H1S_I5_m7_sUp3r_S3CR3t_k3Y';
+describe('Setup Mongodb Memory DB', () => {
+  let mongo;
+  beforeAll(async () => {
+    process.env.JWT_KEY = '7H1S_I5_m7_sUp3r_S3CR3t_k3Y';
 
-  mongo = await MongoMemoryServer.create();
-  const mongoUri = mongo.getUri();
-  await mongoose.connect(mongoUri);
-});
+    mongo = await MongoMemoryServer.create();
+    const mongoUri = mongo.getUri();
+    await mongoose.connect(mongoUri);
+  });
 
-beforeEach(async () => {
-  const collections = await mongoose.connection.db.collections();
+  beforeEach(async () => {
+    const collections = await mongoose.connection.db.collections();
 
-  for (let collection of collections) {
-    await collection.deleteMany({});
-  }
-});
+    for (let collection of collections) {
+      await collection.deleteMany({});
+    }
+  });
 
-afterAll(async () => {
-  await mongo.stop();
-  await mongoose.connection.close();
+  afterAll(async () => {
+    await mongo.stop();
+    await mongoose.connection.close();
+  });
 });
